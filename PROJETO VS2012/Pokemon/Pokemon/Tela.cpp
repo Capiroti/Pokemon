8e212321d8ad2312e5df280ca79e97d10528816d
Alice Abreu <aliiceabreu@gmail.com>
2014-09-13 16:03:55 -0300

    Classe Tela
    Adicionei classe tela

---
+++ PROJETO VS2012/Pokemon/Pokemon/Tela.cpp
@@ -0,0 +1,11 @@
+#include "Tela.h"
+
+
+Tela::Tela()
+{
+}
+
+
+Tela::~Tela()
+{
+}
---
+++ PROJETO VS2012/Pokemon/Pokemon/Tela.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class Tela
+{
+public:
+	Tela();
+	~Tela();
+	virtual int desenhaInterface();
+protected:
+	Imagem fundo;
+};
+
