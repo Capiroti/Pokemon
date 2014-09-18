4aea7c53ff67c23da1d1c04b700a5ad6a1c0dc0a
guilhermern <guilhermerosanunes@hotmail.com>
2014-09-18 20:03:31 -0300

    Add Tilemap e colisao
    noses

--- Projeto C++/PROJETO VS2012/projetos/Pokemon/Jogo.cpp
+++ Projeto C++/PROJETO VS2012/projetos/Pokemon/Jogo.cpp
@@ -13,8 +13,8 @@ void Jogo::controladorTelas()
 	case MENU:
 		//Pergunta pro usuario o sexo e carrega a animacao passando o sexo
 		player.setSexo(GAROTA);
-		mapa->carregar("dados/tilemaps/teste.json");
-		mapa->carregarConfigTileSet("teste", "dados/tilemaps/Tilesets/configRota01.txt");
+		mapa->carregar("dados/tilemaps/rota01.json");
+		mapa->carregarConfigTileSet("TileSetRotas" ,"dados/tilemaps/Tilesets/configRota01.txt");
 		player.carregarAnimacao(mapa->getObjeto("Player"));
 		tela = JOGANDO; // depois do menu começa o jogo;
 		break;
@@ -45,38 +45,39 @@ void Jogo::testPlayerMapa()
 			{
 				if(mapa->tileECaminhavel(player.getXcentral() + 1.0, player.getYcentral()))
 				{
-					player.movePara(player.getXcentral() + 5.0, player.getYcentral());
+					player.movePara(player.getXcentral() + 1.0, player.getYcentral());
 				}
 			}
 			else if(teclado.segurando[TECLA_ESQ])
 			{
 				if(mapa->tileECaminhavel(player.getXcentral() - 1.0, player.getYcentral()))
 				{
-					player.movePara(player.getXcentral() - 10.0, player.getYcentral());
+					player.movePara(player.getXcentral() - 1.0, player.getYcentral());
 				}
 			}
 			else if(teclado.segurando[TECLA_CIMA])
 			{
 				if(mapa->tileECaminhavel(player.getXcentral(), player.getYcentral() - 1.0))
 				{
-					player.movePara(player.getXcentral(), player.getYcentral() - 5.0);
+					player.movePara(player.getXcentral(), player.getYcentral() - 1.0);
 				}
 			}
 			else if(teclado.segurando[TECLA_BAIXO])
 			{
 				if(mapa->tileECaminhavel(player.getXcentral(), player.getYcentral() + 1.0))
 				{
-					player.movePara(player.getXcentral(), player.getYcentral() + 5.0);
+					player.movePara(player.getXcentral(), player.getYcentral() + 1.0);
 				}
 			}
 		}
 
 		//	6)	centraliza o mapa na posicao do player
 	
-		//mapa->setPosCentro(player.getXcentral(), player.getYcentral());
+		mapa->setPosCentro(player.getXcentral(), player.getYcentral()+10);
 		
 		//	7)	desenhar o tilemap (player eh desenhado junto)
 		mapa->desenhar();
+
 }
 
 void Jogo::inicializar()
